2c6de84593566e01b23c543dbbe270f246f1afc1
Zhiwei Han <hanzw356255531@icloud.com>
2016-06-06 14:39:05 +0200

    New modified to motor movement

--- MotorTest.py
+++ MotorTest.py
@@ -9,9 +9,9 @@ print('Using the first on the list', port)
 dxl_io = pypot.dynamixel.DxlIO(port)
 print('Connected!')
 
+# Feasible Angle Test
+angleBefore = list(dxl_io.get_present_position((37, )))[0]
 
-Feasible Angle Test
-angleBefore = list(dxl_io.get_present_position((36, )))[0]
 while 1:
 	if abs(angleBefore - list(dxl_io.get_present_position((37, )))[0]) > 0.5:
 		print angleBefore
